b19e40ba68cb171a11fb86654740e83dcd247af9
o-uki <makaroniaji024@gmail.com>
2025-04-02 21:16:08 +0900

    [update] ine.c

--- ine.c
+++ ine.c
@@ -3,22 +3,53 @@
 
 int in_row;
 
-char run_command(char row_chars[255])
+/* フローコマンドの型と生成する関数 */
+struct flow_command
 {
-    return row_chars[0];
+    char *token[16];
+    char *type[32];
+};
+
+struct flow_command create_flow_command(char set_token[16], char set_type[32])
+{
+    struct flow_command new_command;
+
+    *new_command.token = set_token;
+    *new_command.type = set_type;
+
+    return new_command;
+}
+
+/* Ineを実行する関数 */
+void run_command(char rows[256][128])
+{
+    struct flow_command flow_commands[4] = {
+        create_flow_command("I", "sequence"),
+        create_flow_command("?", "selection")
+    };
+
+    printf("%s", rows[1]);
 }
 
 int main(void)
 {
+    /* ファイル読み込み */
     FILE *filep = fopen("sample.ine", "r");
 
-    char file_row_chars[255];
-    while (fgets(file_row_chars, 255, filep)[strlen(file_row_chars) - 1] == '\n')
+    char source_code_rows[256][128];
+    size_t get_file_row = 0;
+
+    char file_row[128];
+    while (fgets(file_row, 128, filep)[strlen(file_row) - 1] == '\n')
     {
-        printf("%c", run_command(file_row_chars));
+        strcpy(source_code_rows[get_file_row], file_row);
+
+        get_file_row++;
     }
-    fgets(file_row_chars, 255, filep);
-    printf("%s\n", file_row_chars);
+    fgets(file_row, 128, filep);
+    strcpy(source_code_rows[get_file_row], file_row);
+
+    run_command(source_code_rows);
     
     fclose(filep);
 
