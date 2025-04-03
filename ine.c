a887322b96430423ac049c1c6497a2689b361afc
o-uki <makaroniaji024@gmail.com>
2025-04-04 01:18:49 +0900

    [update] ine.c

--- ine.c
+++ ine.c
@@ -2,27 +2,27 @@
 #include <string.h>
 
 /* 見ている行 */
-int in_row;
+int program_count = 1;
 
 /* コマンドの型と生成する関数 */
 struct Command
 {
-    char token[16];
-    char type[32];
+    char* token[16];
+    char* type[32];
 };
 
-struct Command create_command(char set_token[], char set_type[])
+struct Command create_command(char* set_token, char* set_type)
 {
     struct Command new_command;
 
-    *new_command.token = *set_token;
-    *new_command.type = *set_type;
+    *new_command.token = set_token;
+    *new_command.type = set_type;
 
     return new_command;
 }
 
 /* 関数コマンドの実行内容定義 */
-int run_function_command(char type[], int operands[])
+int run_function_command(char* type, int* operands)
 {
     if (strcmp(type, "write") == 0)
     {
@@ -35,12 +35,13 @@ int run_function_command(char type[], int operands[])
 };
 
 /* Ineを実行する関数 */
-void run_commands(char rows[], size_t x_size, size_t y_size)
+void run_commands(char* rows, size_t x_size, size_t y_size)
 {
     /* フローコマンド定義 */
     struct Command flow_commands[] = {
         create_command("I", "sequence"),
-        create_command("?", "selection")
+        create_command("?", "selection"),
+        create_command("+", "connection")
     };
 
     /* 関数コマンド定義 */
@@ -49,27 +50,38 @@ void run_commands(char rows[], size_t x_size, size_t y_size)
         create_command("eq", "equal")
     };
 
+    /* フローコマンドに当てはまるか */
     for (size_t i = 0; i < sizeof flow_commands / sizeof flow_commands[0]; i++)
     {
-        /* 文字列を比較したい文字数切り出す */
+        /* 文字列を比較したい文字数切り出す用の変数 */
         char copy_token[16];
-        for (size_t j = 0; j < strlen(flow_commands[i].token); j++)
+
+        /* 文字列切り出し */
+        for (size_t j = 0; j < strlen(*flow_commands[i].token); j++)
         {
-            copy_token[j] = rows[in_row * x_size + j];
+            copy_token[j] = rows[program_count * x_size + j];
         }
 
-        if (strcmp(flow_commands[i].token, copy_token))
+        if (strcmp(*flow_commands[i].token, copy_token) == 0)
         {
-            if (strcmp(flow_commands[i].type, "sequence"))
+            if (strcmp(*flow_commands[i].type, "sequence") == 0)
             {
-                for (size_t j = 0; j < strlen(copy_token); j++)
-                {
-                    copy_token[j] = '\0';
-                }
-
+                /* 関数コマンドに当てはまるか */
                 for (size_t j = 0; j < sizeof function_commands / sizeof function_commands[0]; j++)
                 {
-                    
+                    /* 文字列を比較したい文字数切り出す用の変数リセット */
+                    for (size_t k = 0; k < strlen(copy_token); k++)
+                    {
+                        copy_token[k] = '\0';
+                    }
+
+                    /* 文字列切り出し */
+                    for (size_t k = 0; k < strlen(*function_commands[j].token); k++)
+                    {
+                        copy_token[k] = rows[program_count * x_size + k + strlen(*flow_commands[i].token) + 1];
+                    }
+
+                    /* 引数を */
                 }
             }
         }
