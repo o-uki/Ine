34ea63a017c20a049da08a80f3d19ead3c07f9dd
o-uki <makaroniaji024@gmail.com>
2025-04-10 00:54:19 +0900

    [update] ine/main.c sample.ine

--- ine/main.c
+++ ine/main.c
@@ -1,6 +1,4 @@
 #include "../file.h"
-#include "./function.h"
-#include "./token.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,6 +6,19 @@
 /* ソースコードの最大行列数 */
 #define MAX_ROW_SIZE 256
 #define MAX_COLUMN_SIZE 256
+
+/* 文字列切り出し関数 */
+char* string_subtract(char* string, char* copy_string, int start_index, int length);
+
+/* エラー処理関数 */
+void get_error();
+
+/* コマンドの型 */
+struct Token
+{
+    char *token;
+    char *code;
+};
 
 int main(void)
 {
@@ -22,16 +33,30 @@ int main(void)
     size_t program_count = 0;
 
     /* フローコマンドの配列 */
-    struct Command flow_commands[] = {
+    struct Token flow_commands[] = {
         {.token = "I", .code = "sequence"},
         {.token = "?", .code = "selection"}
     };
 
     /* 関数コマンドの配列 */
-    struct Command function_commands[] = {
+    struct Token function_commands[] = {
         {.token = "wr", .code = "write"},
         {.token = "eq", .code = "equal"}
     };
+
+    /* 接中辞の配列 */
+    struct Token infix[] = {
+        {.token = ",", .code = "separate"},
+        {.token = "+", .code = "plus"}
+    };
+    
+    /* 接頭辞の配列 */
+    struct Token prefix[] = {
+        {.token = "H", .code = "hexadecimal"},
+        {.token = "B", .code = "binary"},
+        {.token = "#", .code = "string"},
+    };
+    
 
     /* ファイル読み込み */
     for (size_t i = 0; fgets(source_code[i], MAX_COLUMN_SIZE, file_point) != NULL; i++);
@@ -45,13 +70,16 @@ int main(void)
             {
                 if (strcmp(flow_commands[i].code, "sequence") == 0)
                 {
+                    /* 順次実行 */
                     for (size_t j = 0; j < sizeof function_commands / sizeof function_commands[0]; j++)
                     {
-                        if (strcmp(string_subtract(source_code[program_count], copy_string, flow_commands[i].token + 1, strlen(function_commands[j].token)), function_commands[j].token))
+                        if (strcmp(string_subtract(source_code[program_count], copy_string, strlen(flow_commands[i].token) + 1, strlen(function_commands[j].token)), function_commands[j].token) == 0)
                         {
-                            if (source_code[program_count][strlen(flow_commands[i].token)] = ' ')
+                            if (source_code[program_count][strlen(flow_commands[i].token)] == ' ')
                             {
-                                /* code */
+                                
+                                
+                                break;
                             } else
                             {
                                 /* フローコマンドと関数コマンドの間に空白がなければエラー */
@@ -59,15 +87,15 @@ int main(void)
                             }
                         }
                     }
-                }
 
-                continue;
+                    program_count++;
+                    break;
+                }
             } else if (i + 1 < sizeof flow_commands / sizeof flow_commands[0])
             {
                 /* フローコマンドが認識されなかったときのエラー */
                 get_error();
             }
-            
         }
     }
 
@@ -92,8 +120,31 @@ char* string_subtract(char* string, char* copy_string, int start_index, int leng
     return copy_string;
 }
 
+/* エラー処理関数 */
 void get_error()
 {
     printf("Error!!\n");
     exit(EXIT_FAILURE);
+}
+
+/* 引数を区切る */
+void separate_arguments(struct Token* infixes, char* argument_string, char* fix_arugments)
+{
+    size_t infix_index;
+    char copy_string[64];
+
+    for (size_t i = 0; i < sizeof *infixes / sizeof infixes[0]; i++)
+    {
+        if (strcmp(infixes[i].code, "separate") == 0)
+        {
+            infix_index = i;
+
+            break;
+        }
+    }
+    
+    for (size_t i = 0; i < strlen(argument_string); i += strlen(infixes[infix_index].token))
+    {
+        
+    }
 }
