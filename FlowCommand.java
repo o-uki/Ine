6febac2f756264cc55a9587c0024d87834dc5f48
o-uki <makaroniaji024@gmail.com>
2025-04-15 23:30:35 +0900

    ファイル名変更しました。順序記号の配列を作りました。

---
+++ FlowCommand.java
@@ -0,0 +1,9 @@
+class FlowCommand {
+    public String token;
+    public String name;
+
+    public FlowCommand(String setToken, String setName) {
+        this.token = setToken;
+        this.name = setName;
+    }
+}
--- IneRun.java
+++ IneRun.java
@@ -1,14 +1,26 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-// import java.util.function.Function;
+import java.util.function.Function;
 import java.util.List;
 
 class IneRun {
     public static void main(String[] args) {
         try {
+            // ファイル読み込み
             List<String> sourceCode = Files.readAllLines(Path.of("sample.ine"));
             System.out.print(sourceCode + "\n");
+
+            // PC
+            int programCount = 0;
+
+            // 順序記号の配列
+            FlowCommand flowCommands[] = new FlowCommand[3];
+            flowCommands[0] = new FlowCommand("I", "sequence");
+            flowCommands[1] = new FlowCommand("?", "selection");
+            flowCommands[2] = new FlowCommand("+", "join");
+
+            System.out.print(flowCommands[0].name);
 
             // Function<String, Integer> runner = (str) -> {
             //     System.out.print(Integer.parseInt(str));
